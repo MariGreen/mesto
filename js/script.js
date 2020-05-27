b3567e69aa7cfa94258f10a13dfae47259f97658
Marina Grishina <marisupova@ya.ru>
2020-05-28 00:18:38 +0300

    onePPandOther

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ const profileVocation = document.querySelector('.profile__vocation');
 const popupProfileVocation = document.querySelector('.popup__form-item-field_vocation');
 const formElement = document.querySelector('.popup__form-container');
 const editUser = document.querySelector('.profile__edit-button');
-const changeMyMind = document.querySelector('.popup__close-button');
+const closeButton = document.querySelector('.popup__close-button');
 
-const testProfileListener = document.querySelector('.profile');
 
-  //const popupProfilePlace = document.querySelector('.popup__form-item-field_place');
-  //const popupProfileLink = document.querySelector('.popup__form-item-field_link');
-  const popupHeader = document.querySelector('.popup__edit-profile');
-  const saveButton = document.querySelector('.popup__save-button');
+const popupHeader = document.querySelector('.popup__edit-profile');
+const saveButton = document.querySelector('.popup__save-button');
 
-  const newCard = document.querySelector('.profile__add-button');
+const newCard = document.querySelector('.profile__add-button');
+
+const elements = document.querySelector('.elements');
+
   
 
   const initialCards = [
@@ -44,71 +44,119 @@ const testProfileListener = document.querySelector('.profile');
     }
   ];
 
-function getName (evt) {
-  
-  if (evt.target === editUser) {
-    console.log('yey');
-    popupProfileName.value = profileName.textContent;
-    popupProfileName.placeholder = 'Имя';  
-    popupProfileVocation.value = profileVocation.textContent;
-    popupProfileVocation.placeholder = 'О себе';
-    popupHeader.textContent = 'Редактировать профиль';
-    saveButton.textContent = 'Сохранить';
-  }
-   else if (evt.target === newCard) {
-    popupProfileName.value = '';
-    popupProfileName.placeholder = 'Название';  
-    popupProfileVocation.value = '';
-    popupProfileVocation.placeholder = 'Ссылка';
-    popupHeader.textContent = 'Новое место';
-    saveButton.textContent = 'Создать';
-   }
-    else {
-      console.log('nope');
-    }
-  
-
+function getName () {
+  popupProfileName.value = profileName.textContent;
+  popupProfileVocation.value = profileVocation.textContent;
 }
 
-    function addCard () {
 
-    }
+
+
+function profileTemlate () {
+  popupProfileName.placeholder = 'Имя';  
+  popupProfileVocation.placeholder = 'О себе';
+  popupHeader.textContent = 'Редактировать профиль';
+  saveButton.textContent = 'Сохранить';
+}
+
+function cardTemlate () {
+  popupProfileName.value = '';
+  popupProfileName.placeholder = 'Название';  
+  popupProfileVocation.value = '';
+  popupProfileVocation.placeholder = 'Ссылка';
+  popupHeader.textContent = 'Новое место';
+  saveButton.textContent = 'Создать';
+}
 
 function openClose (evt) {
-  if ((evt.target === editUser) || (evt.target === newCard) || (evt.target === changeMyMind) || (evt.target === saveButton)) {
+ //if ((evt.target === editUser) || (evt.target === newCard) || (evt.target === changeMyMind) || (evt.target === saveButton)) {
     if (popup.classList.contains('popup_opened')) { 
       popup.classList.remove('popup_opened');
     } else {
+      if (evt.target.classList.contains('profile__edit-button')) {
+      // заполняем попап шаблоном профиля
+      profileTemlate (); 
+      } else if (evt.target.classList.contains('profile__add-button')) {
+        // заполняем попап шаблоном карточки
+        cardTemlate ();
+      }      
       popup.classList.add('popup_opened');
-      getName (evt);
     }
   }
+
+function saveProfile () {
+  // Получите значение полей из свойства value
+  const newName = popupProfileName.value;
+  const newJob = popupProfileVocation.value;
+    // Вставьте новые значения с помощью textContent
+  profileVocation.textContent = newJob;
+  profileName.textContent = newName;
+}
+
+function render () {
+  initialCards.forEach(function(item) {
+    const elementTemplate = document.querySelector('.element_template').content;
+    const element = elementTemplate.cloneNode(true);
+    const elementPicture = element.querySelector('.element__picture');
+    const elementPlace = element.querySelector('.element__place');
+    elementPicture.src = item.link;
+    elementPlace.textContent = item.name;
+    elements.append(element);
+  })
+}
+render ();
+
+
+
+function addCard () {
+  const elementTemplate = document.querySelector('.element_template').content;
+  const element = elementTemplate.cloneNode(true);
+  elements.prepend(element);
+  const newCardPicture = document.querySelector('.element__picture');
+  const newCardPlace = document.querySelector('.element__place');
+  newCardPicture.src = popupProfileVocation.value;
+  newCardPlace.textContent = popupProfileName.value;
 }
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                              // Так мы можем определить свою логику отправки.
-                                              // О том, как это делать, расскажем позже.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
 
-  // Получите значение полей из свойства value
-  const newName = popupProfileName.value;
-  const newJob = popupProfileVocation.value;
-  // Вставьте новые значения с помощью textContent
-  profileVocation.textContent = newJob;
-  profileName.textContent = newName;
-  openClose(evt);
+  // смотрим какой попап открыт
+
+  if (popupHeader.textContent === 'Редактировать профиль') {
+  //функция сохранения профиля
+  saveProfile ();
+
+  } else if (popupHeader.textContent === 'Новое место') {
+  //функция сохранения карточки
+  addCard ();
+  }
+  openClose (evt);  
+  
 }
 
-changeMyMind.addEventListener('click', openClose);
-testProfileListener.addEventListener('click', function(evt) {
+closeButton.addEventListener('click', openClose);
+
+//testProfileListener.addEventListener('click', function(evt) {
+//  openClose(evt);
+//});
+
+//редактирование профиля
+editUser.addEventListener('click', function(evt) {
+  getName();
   openClose(evt);
 });
 
-//editUser.addEventListener('click', function(evt) {
-//  openClose(evt);
-//});
+//добавление картинки
+newCard.addEventListener('click', function(evt) {
+  openClose(evt);
+});
+
+
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
